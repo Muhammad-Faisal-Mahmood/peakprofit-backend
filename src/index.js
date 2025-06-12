bf07e6b51b064b2a9205477125371b095bec4b7e
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2025-06-13 01:08:15 +0500

    FEAT: User & Auth apis

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+
+const path = require("path"); // Add this line
+
+require("dotenv").config({
+  path: path.resolve(__dirname, "..", ".env"), // Go up one directory from src
+});
+
+const auth = require("./auth/auth.controller");
+const user = require("./user/user.controller");
+
+const app = express();
+const PORT = process.env.PORT; // Use PORT from .env or default to 3000
+console.log("port: ", PORT);
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // Consider restricting this to your frontend's actual origin in production
+  })
+);
+
+// Passport
+app.use(
+  session({
+    secret: process.env.PASSPORT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use("/api/auth", auth);
+app.use("/api/user", user);
+
+// MongoDB Connection
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit process if MongoDB connection fails
+  }
+};
+
+// Connect to MongoDB and then start the server
+connectToMongoDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
