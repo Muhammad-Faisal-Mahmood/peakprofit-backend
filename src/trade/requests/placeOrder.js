b2b26f95b27fad27a1e41ded9574de2b411e3145
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2025-11-17 23:07:23 +0500

    error msg changed of place order

--- src/trade/requests/placeOrder.js
+++ src/trade/requests/placeOrder.js
@@ -150,10 +150,7 @@ const placeOrder = async (req, res) => {
     }
   } catch (err) {
     console.error("Error placing order:", err);
-    return sendErrorResponse(
-      res,
-      "Failed to place order. REASON: " + err.message
-    );
+    return sendErrorResponse(res, "Failed to place order.");
   }
 };
 
