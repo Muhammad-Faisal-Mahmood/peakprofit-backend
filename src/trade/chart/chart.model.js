c9ca315241555b1d07b6b40b479535214c71c8b8
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2025-11-24 04:49:28 +0500

    chart intervals changed

--- src/trade/chart/chart.model.js
+++ src/trade/chart/chart.model.js
@@ -4,7 +4,26 @@ const ChartSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", unique: true },
   interval: {
     type: String,
-    enum: ["1", "5", "15", "30", "60", "240", "1D", "1W", "1M"],
+    enum: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "10",
+      "15",
+      "30",
+      "60",
+      "120",
+      "240",
+      "300",
+
+      "1D",
+      "2D",
+      "3D",
+      "1W",
+      "1M",
+    ],
     default: "1",
   },
 });
